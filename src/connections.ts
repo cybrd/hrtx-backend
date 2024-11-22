ff32bb8c52752e6d4cdd4414eae704660496f5e1
cybrd <brysuralta@gmail.com>
2024-11-22 16:28:17 +0800

    Update connections.ts

--- src/connections.ts
+++ src/connections.ts
@@ -3,7 +3,12 @@ import pgpClass from "pg-promise";
 import { config } from "dotenv";
 config();
 
-export const db = pgpClass()({
+const pgp = pgpClass();
+
+const INT8 = 20;
+pgp.pg.types.setTypeParser(INT8, parseInt);
+
+export const db = pgp({
   host: process.env.DB_HOST,
   password: process.env.DB_PASSWORD,
   port: 5432,
